af760fbfaef79c0081f83045f4db41482120695d
Sliver1de <71007801+Sliver1de@users.noreply.github.com>
2026-08-22 20:59:12 +0800

    修改PlayerControl代码，增加接收输入

--- Assets/Scrpts/Player/PlayerControl.cs
+++ Assets/Scrpts/Player/PlayerControl.cs
@@ -3,14 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControl : MonoBehaviour, IStateMachineOwner
 {
     [SerializeField] private Weapon weapon;
+
+    [SerializeField] private StateMachine _stateMachine;
+    
+    public Vector2 MoveInput { get; private set; }
     
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        _stateMachine = new StateMachine(this);
+        _stateMachine.AddState(new PlayerIdleState());
+        _stateMachine.AddState(new PlayerMoveState());
+        
+        _stateMachine.ChangeState<PlayerIdleState>();
     }
 
     private void Update()
@@ -19,5 +29,14 @@ public class PlayerControl : MonoBehaviour
         {
             weapon.Fire();
         }
+        
+        ReadInput();
+        
+        _stateMachine.Update();
+    }
+
+    private void ReadInput()
+    {
+        MoveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 }
